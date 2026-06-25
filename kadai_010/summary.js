4ac62098915777979e57f753eef09c80ea96b1a5
yasu6kayo <3255kynyan@gmail.com>
2026-06-25 13:54:17 +0900

    課題を提出

---
+++ kadai_010/summary.js
@@ -0,0 +1,15 @@
+$('#change-color').on('click', function() {
+  $('#target').css('color', 'red');
+});
+
+$('#change-text').on('click', function() {
+  $('#target').text('こんばんは！');
+});
+
+$('#fade-out').on('click', function() {
+  $('#target').fadeOut();
+});
+
+$('#fade-in').on('click', function() {
+  $('#target').fadeIn();
+});
